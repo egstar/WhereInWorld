d74731658e1404c74ca10c6e9863f21da8bf5a54
Burham B. Soliman <Borham@hotmail.com>
2023-12-04 18:32:26 +0200

    remove CORS

--- src/pages/api/countries.tsx
+++ src/pages/api/countries.tsx
@@ -7,34 +7,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if(req.method === 'GET'){
         try {
             let countries = await Country.getAll()
-            console.error(countries[0])
-            if(countries)
-            res.status(200)
-            .json({data: countries})
+            if(countries){
+                res.status(200).json({
+                    data: countries
+                })
+            } else {
+                res.status(300).json({
+                    error: 'No data'
+                })
+            }
         } catch(e) {
-            res.status(401).json({error: 'Not Authorized'})
+            res.status(401).json({
+                error: 'Not Authorized'
+            })
         }
     }
     if(req.method === 'POST' && req.body.country) {
         try {
             let country = await Country.getCountry(req.body.country!)
             if(country){
-                res.status(200).json({data: country})
+                res.status(200).json({
+                    data: country
+                })
             }
         } catch(e) {
-            res.status(401).json({error: 'Not Authorized'})
+            res.status(401).json({
+                error: 'Not Authorized'
+            })
         }
     }
     if(req.method === 'OPTIONS' && req.body.borders) {
         let {borders} = req.body
         let bordersNames = await Country.getBorders(borders)
         try{
-            res.status(200).json({data: bordersNames})
+            res.status(200).json({
+                data: bordersNames
+            })
         } catch(e) {
-            res.status(401).json({error: 'Not Authorized'})
+            res.status(401).json({
+                error: 'Not Authorized'
+            })
         }
     }
-    res.status(403).json({
+    res.status(302).json({
         Done:false,
         Host: req.headers.host,
         origin: req.headers.origin || 'EMPTY',
