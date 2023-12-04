f1a65b14ea73e88c71aa4e494c23d6dca05160f0
Burham B. Soliman <Borham@hotmail.com>
2023-12-04 18:10:50 +0200

    remove CORS

--- src/pages/api/countries.tsx
+++ src/pages/api/countries.tsx
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if(req.method === 'GET'){
         try {
             let countries = await Country.getAll()
-            if(countries)
+            if(!countries) res.json('Failed')
             res.status(200)
             .json({countries: countries})
         } catch(e) {
